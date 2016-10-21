739120a5fde91d839ef187e4940b288ab5569312
kuylim <kuylim.tith@gmail.com>
2016-10-21 14:13:11 +0700

    fix bugs

--- app/repos/impl/TranslatorRepoImpl.scala
+++ app/repos/impl/TranslatorRepoImpl.scala
@@ -1,42 +1,25 @@
-package repos
+package repos.impl
 
 import javax.inject.Inject
 
-import models.Translator
 import api.{ FilterData, Pagination }
-import com.google.inject.ImplementedBy
+import models.Translator
 import play.api.libs.json.{ JsObject, Json }
 import play.modules.reactivemongo.ReactiveMongoApi
-import play.modules.reactivemongo.json._
-import reactivemongo.api.{ QueryOpts, ReadPreference }
 import reactivemongo.api.commands.WriteResult
+import reactivemongo.api.{ QueryOpts, ReadPreference }
 import reactivemongo.bson.BSONDocument
 import reactivemongo.play.json.collection.JSONCollection
+import repos.TranslatorRepo
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.modules.reactivemongo.json._
 
 import scala.concurrent.{ ExecutionContext, Future }
 
 /**
- * Created by KUYLIM on 10/12/2016.
+ * Created by Kuylim on 10/21/2016.
  */
-@ImplementedBy(classOf[TranslatorRepoImpl])
-trait TranslatorRepository {
-  def find(pagination: Pagination, filterData: FilterData)(implicit ec: ExecutionContext): Future[List[JsObject]]
-
-  def select(id: BSONDocument)(implicit ec: ExecutionContext): Future[Option[JsObject]]
-
-  def update(id: BSONDocument, update: Translator)(implicit ec: ExecutionContext): Future[WriteResult]
-
-  def remove(id: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult]
-
-  def save(document: Translator)(implicit ec: ExecutionContext): Future[WriteResult]
-
-  def count()(implicit ec: ExecutionContext): Future[Int]
-
-}
-
-/*Implement class*/
-
-class TranslatorRepoImpl @Inject() (reactiveMongoApi: ReactiveMongoApi) extends TranslatorRepository {
+class TranslatorRepoImpl @Inject() (reactiveMongoApi: ReactiveMongoApi) extends TranslatorRepo {
 
   def collection(implicit ec: ExecutionContext) = reactiveMongoApi.database.map(_.collection[JSONCollection]("TRANSLATORS"))
 
