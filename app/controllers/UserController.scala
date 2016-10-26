c40305d80e8e500c5f852ad434343de6b1b9b1f0
Thanak <nganthank@2ntkh.com>
2016-10-26 10:01:51 +0700

    change user controller

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,67 +1,44 @@
 package controllers
 
 import javax.inject.Inject
-
-import api.FilterData
+import api.{ FilterData, Pagination, WrappJson }
 import com.difflang.models.User1
-import models.{ Pagination, WrappJson }
+import play.api.i18n.MessagesApi
 import play.api.libs.json.Json
-import play.api.mvc._
-import play.modules.reactivemongo.{ MongoController, ReactiveMongoApi, ReactiveMongoComponents }
-import reactivemongo.bson.{ BSONDocument, BSONObjectID }
 import repos.UserRepository
-
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.Try
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
 /**
- * Created by acer on 10/11/2016.
+ * Created by Thanak on 10/11/2016.
  */
-class UserController @Inject() (val reactiveMongoApi: ReactiveMongoApi, userRepo: UserRepository) extends Controller with MongoController with ReactiveMongoComponents {
+class UserController @Inject() (val userService: UserRepository, val messagesApi: MessagesApi) extends api.ApiController {
 
-  //def userRepo = new UserRepositoryImpl(reactiveMongoApi)
-
-  def index = Action.async {
-    implicit request =>
-
-      userRepo.find().map(users => Ok(Json.toJson(users)))
-  }
-
-  def create = Action.async(BodyParsers.parse.json) { implicit request =>
+  def create = SecuredApiActionWithBody { implicit request =>
     val user = (request.body).as[User1]
-
-    userRepo.save(user).map(result => Created)
+    userService.save(user).flatMap(result => created())
   }
 
-  def read(id: String) = Action.async {
-    val OId: Try[BSONObjectID] = BSONObjectID.parse(id)
-    userRepo.select(BSONDocument("_id" -> OId.get)).map(user => Ok(Json.toJson(user)))
+  def findById(id: String) = SecuredApiAction { implicit request =>
+    userService.select(id).flatMap(translator => ok(Json.toJson(translator)))
   }
 
-  def update(id: String) = Action.async(BodyParsers.parse.json) {
-    {
-      implicit request =>
-        val user = (request.body).as[User1]
-        val OId: Try[BSONObjectID] = BSONObjectID.parse(id)
-        val selector = BSONDocument("_id" -> OId.get)
-
-        val modifier = (user)
-        userRepo.update(selector, modifier).map(result => Accepted)
-    }
+  def update(id: String) = SecuredApiActionWithBody {
+    implicit request =>
+      val user = (request.body).as[User1]
+      userService.update(id, user).flatMap(result => accepted())
   }
 
-  def delete(id: String) = Action.async {
-    val OId: Try[BSONObjectID] = BSONObjectID.parse(id)
-    userRepo.remove(BSONDocument("_id" -> OId.get)).map(result => Accepted)
+  def delete(id: String) = SecuredApiAction { implicit request =>
+    userService.remove(id).flatMap(_ => noContent())
   }
 
-  def findPaginate(sort: String, page: Int, limit: Int) = Action.async {
+  def findAll(sort: String, page: Int, limit: Int) = SecuredApiAction { implicit request =>
     val sortData = new FilterData(sort)
-    val number1 = Await.result(userRepo.count1(), 10 seconds)
+    val number1 = Await.result(userService.count(), 10 seconds)
     val pagination = new Pagination(page, limit, number1)
-    userRepo.findByPaginate(pagination, sortData).map(users => Ok(Json.toJson(WrappJson(users, pagination))))
+    userService.findAll(pagination, sortData).flatMap(users => ok(Json.toJson(WrappJson(users, pagination))))
   }
 
 }
--- app/repos/impl/UserRepositoryImpl.scala
+++ app/repos/impl/UserRepositoryImpl.scala
@@ -2,18 +2,18 @@ package repos.impl
 
 import javax.inject.Inject
 
-import api.FilterData
+import api.{ FilterData, Pagination }
 import com.difflang.models.User1
-import models.Pagination
 import play.api.libs.json.{ JsObject, Json }
 import play.modules.reactivemongo.ReactiveMongoApi
 import play.modules.reactivemongo.json._
 import reactivemongo.api.commands.WriteResult
 import reactivemongo.api.{ QueryOpts, ReadPreference }
-import reactivemongo.bson.BSONDocument
 import reactivemongo.play.json.collection.JSONCollection
 import repos.UserRepository
+import utils.BSONObjectIDConverter
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{ ExecutionContext, Future }
 
 /**
@@ -23,38 +23,30 @@ class UserRepositoryImpl @Inject() (reactiveMongoApi: ReactiveMongoApi) extends 
 
   def collection(implicit ec: ExecutionContext) = reactiveMongoApi.database.map(db => db.collection[JSONCollection]("USERS"))
 
-  override def find()(implicit ec: ExecutionContext): Future[List[JsObject]] = {
-    val genericQueryBuilder = collection.map(_.find(Json.obj()))
-    val cursor = genericQueryBuilder.map(_.cursor[JsObject](ReadPreference.primary))
-    cursor.flatMap(_.collect[List]())
-
+  override def update(id: String, update: User1)(implicit ec: ExecutionContext): Future[WriteResult] = {
+    collection.flatMap(_.update(BSONObjectIDConverter(id).selector, update))
   }
 
-  override def update(id: BSONDocument, update: User1)(implicit ec: ExecutionContext): Future[WriteResult] = {
-    collection.flatMap(_.update(id, update))
+  override def remove(id: String)(implicit ec: ExecutionContext): Future[WriteResult] = {
+    collection.flatMap(_.remove(BSONObjectIDConverter(id).selector))
   }
 
-  override def remove(id: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult] = {
-    collection.flatMap(_.remove(id))
-  }
-
-  override def select(id: BSONDocument)(implicit ec: ExecutionContext): Future[Option[JsObject]] = {
-    collection.flatMap(_.find(id).one[JsObject])
+  override def select(id: String)(implicit ec: ExecutionContext): Future[Option[JsObject]] = {
+    collection.flatMap(_.find(BSONObjectIDConverter(id).selector).one[JsObject])
   }
 
   override def save(document: User1)(implicit ec: ExecutionContext): Future[WriteResult] = {
     collection.flatMap(_.insert(document))
   }
 
-  override def findByPaginate(pagination: Pagination, filterData: FilterData)(implicit ec: ExecutionContext): Future[List[JsObject]] = {
-    val genericQueryBuilder = collection.map(_.find(Json.obj()).options(QueryOpts(pagination.OFFSET)))
+  override def findAll(pagination: Pagination, filterData: FilterData)(implicit ec: ExecutionContext): Future[List[JsObject]] = {
+    val genericQueryBuilder = collection.map(_.find(Json.obj()).options(QueryOpts(pagination.skip)))
     val cursor = genericQueryBuilder.map(_.cursor[JsObject](ReadPreference.Primary))
-    cursor.flatMap(_.collect[List](pagination.LIMIT))
+    cursor.flatMap(_.collect[List](pagination.Size))
   }
 
-  def count1()(implicit ec: ExecutionContext): Future[Int] = {
-    val num = collection.flatMap(_.count())
-    num
+  def count()(implicit ec: ExecutionContext): Future[Int] = {
+    collection.flatMap(_.count())
   }
 
 }
